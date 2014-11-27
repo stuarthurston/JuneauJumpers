d0f68ee5b63dbc9c05ab99a154db2582ed9cb33c
Stuart Thurston <Stuart.Thurston@uvm.edu>
2014-11-26 21:39:47 -0700

    The or clause can be added to delete multiple rows

--- SQL to delete from relation.sql
+++ SQL to delete from relation.sql
@@ -2,9 +2,12 @@ DELETE FROM tblAttends
 WHERE fnkMemberId IN (
     SELECT a.fnkMemberId
     FROM (SELECT * FROM tblAttends) a 
-    WHERE a.fnkMemberId='0' 
+    WHERE a.fnkMemberId='0'
+    AND a.fnkMemberId='0'
 ) AND fnkEventId IN (
     SELECT b.fnkEventId
     FROM (SELECT * FROM tblAttends) b 
     WHERE b.fnkEventId='12' 
+    OR b.fnkEventId='37'
+    OR b.fnkEventId='36'
 );
