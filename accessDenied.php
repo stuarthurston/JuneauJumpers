ad23d3412399b23782990931defcd19e487bac4b
Stuart Thurston <Stuart.Thurston@uvm.edu>
2014-11-29 19:11:37 -0700

    Create a file to deal with an invalid login

---
+++ accessDenied.php
@@ -0,0 +1,13 @@
+<aside class="accessDenied">
+    <h2>
+        ACCESS DENIED
+    </h2>
+
+    <form action ="loginPage.php">
+        <fieldset class="button">
+            <input type="submit" value="Log In" tabindex="900" class="button">
+        </fieldset>
+    </form>
+
+
+</aside>
